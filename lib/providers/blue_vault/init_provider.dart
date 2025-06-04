131e6de9552f50c1dfb903d37e80dc4ba7ff8aa9
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-06-03 20:50:44 -0700

    add init provider

---
+++ lib/providers/blue_vault/init_provider.dart
@@ -0,0 +1,117 @@
+// Init provider implements startup stages.
+
+import 'dart:developer';
+
+import 'package:zxbase_app/providers/blue_vault/blue_vault_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zxbase_flutter_ui/zxbase_flutter_ui.dart';
+import 'package:zxbase_vault/zxbase_vault.dart';
+
+const _component = 'initProvider'; // logging component
+
+final initProvider = StateNotifierProvider<InitNotifier, Init>(
+  (ref) => InitNotifier(ref),
+);
+
+class InitNotifier extends StateNotifier<Init> {
+  InitNotifier(this.ref) : super(Init());
+  final Ref ref;
+  static const _docName = 'init';
+
+  // init has to be called by startup sequence
+  Future<void> init() async {
+    Doc? doc = await ref.read(blueVaultProvider).getDoc(name: _docName);
+    if (doc == null) {
+      log('Create new doc.', name: _component);
+      await ref
+          .read(blueVaultProvider)
+          .updateDoc(name: _docName, content: state.toJson(), annotation: {});
+    } else {
+      log('Load existing doc.', name: _component);
+      state = Init.fromJson(doc.content);
+    }
+  }
+
+  Future<bool> updateDoc(Init newState) async {
+    Doc? doc = await ref
+        .read(blueVaultProvider)
+        .updateDoc(name: _docName, content: newState.toJson(), annotation: {});
+    if (doc == null) {
+      return false;
+    }
+    state = newState; // trigger notification
+    return true;
+  }
+
+  Future<bool> setWizardStage(String val) async {
+    Init stateCopy = Init.copy(state);
+    stateCopy.wizardStage = val;
+    return await updateDoc(stateCopy);
+  }
+
+  Future<bool> setTheme(String val) async {
+    Init stateCopy = Init.copy(state);
+    stateCopy.theme = val;
+    return await updateDoc(stateCopy);
+  }
+
+  Future<bool> setAttempts(int val) async {
+    Init stateCopy = Init.copy(state);
+    stateCopy.attempts = val;
+    stateCopy.attemptDate = DateTime.now().millisecondsSinceEpoch;
+    return await updateDoc(stateCopy);
+  }
+
+  Future<bool> setCameraPermissionInfoShowed() async {
+    Init stateCopy = Init.copy(state);
+    stateCopy.cameraPermissionInfoShowed = true;
+    return await updateDoc(stateCopy);
+  }
+}
+
+// pure data model, nothing provider-related
+class Init {
+  Init();
+
+  Init.copy(Init copy) {
+    wizardStage = copy.wizardStage;
+    theme = copy.theme;
+    attempts = copy.attempts;
+    cameraPermissionInfoShowed = copy.cameraPermissionInfoShowed;
+  }
+
+  // doc deserialization
+  Init.fromJson(Map<String, dynamic> parsedJson) {
+    wizardStage = parsedJson['wizardStage'];
+    theme = parsedJson['theme'];
+    cameraPermissionInfoShowed =
+        parsedJson['cameraPermissionInfoShowed'] ?? false;
+    attempts = parsedJson['attempts'] ?? 0;
+    attemptDate = parsedJson['attemptDate'] ?? 0;
+  }
+
+  // use constants, avoid enums, they are not easy serializable
+
+  // wizard progress stages
+  static const none = 'none';
+  static const vaultInitialized = 'vaultInitialized';
+  static const deviceRegistered = 'deviceRegistered';
+  static const completed = 'completed';
+
+  String wizardStage = none;
+  String theme = AppTheme.light;
+  int attempts = 0;
+  int attemptDate = 0;
+  bool cameraPermissionInfoShowed = false;
+
+  // doc serialization
+  Map<String, dynamic> toJson() {
+    return {
+      'wizardStage': wizardStage,
+      'theme': theme,
+      'attempts': attempts,
+      'attemptDate': attemptDate,
+      'cameraPermissionInfoShowed': cameraPermissionInfoShowed,
+    };
+  }
+}
---
+++ test/providers/blue_vault/init_provider_test.dart
@@ -0,0 +1,75 @@
+import 'package:zxbase_app/providers/blue_vault/blue_vault_provider.dart';
+import 'package:zxbase_app/providers/blue_vault/init_provider.dart';
+import 'package:zxbase_app/providers/config_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zxbase_flutter_ui/zxbase_flutter_ui.dart';
+import '../../helpers.dart';
+
+void main() {
+  cleanupDb();
+  mockPathProvider();
+
+  test('Get the doc from a new vault', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(blueVaultProvider.notifier).init();
+    await container.read(initProvider.notifier).init();
+
+    // read the doc
+    var init = container.read(initProvider);
+    expect(init.wizardStage, equals(Init.none));
+
+    // set run state
+    bool rv = await container
+        .read(initProvider.notifier)
+        .setWizardStage(Init.vaultInitialized);
+    expect(rv, equals(true));
+
+    // set the theme
+    rv = await container.read(initProvider.notifier).setTheme(AppTheme.dark);
+    expect(rv, equals(true));
+
+    // read attempts
+    int attempts = container.read(initProvider).attempts;
+    expect(attempts, equals(0));
+
+    // set attempts
+    rv = await container.read(initProvider.notifier).setAttempts(1);
+    expect(rv, equals(true));
+
+    // mark camera permission
+    rv = await container
+        .read(initProvider.notifier)
+        .setCameraPermissionInfoShowed();
+    expect(rv, equals(true));
+  });
+
+  test('Get existing doc', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(blueVaultProvider.notifier).init();
+    await container.read(initProvider.notifier).init();
+
+    // read the doc
+    var init = container.read(initProvider);
+    expect(init.wizardStage, equals(Init.vaultInitialized));
+    expect(init.theme, equals(AppTheme.dark));
+
+    // get state
+    var state = container.read(initProvider);
+    expect(state.theme, equals(AppTheme.dark));
+
+    // read attempts
+    int attempts = container.read(initProvider).attempts;
+    expect(attempts, equals(1));
+  });
+}

2074be2de4ccd9facf78045c953b7ee8aab13af4
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-06-09 18:03:47 -0700

    add user vault provider

---
+++ lib/providers/green_vault/user_vault_provider.dart
@@ -0,0 +1,309 @@
+// User vault provider.
+// Keeps all local records: logins, notes e.t.c.
+// All entries adressed by entries map: UUID -> UserVaultEntry
+// Additionally, entries are addressed by ephemeral group maps:
+//   Logins: UUID -> True
+//   Notes: UUID -> True
+
+import 'dart:convert';
+import 'dart:developer';
+import 'package:zxbase_app/core/ui/app_err.dart';
+import 'package:zxbase_app/providers/config_provider.dart';
+import 'package:zxbase_app/providers/green_vault/device_provider.dart';
+import 'package:zxbase_app/providers/green_vault/green_vault_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zxbase_vault/zxbase_vault.dart';
+
+const _comp = 'userVaultProvider'; // logging component
+
+final userVaultProvider = StateNotifierProvider<UserVaultNotifier, UserVault>(
+  (ref) => UserVaultNotifier(
+    ref: ref,
+    deviceId: ref.read(deviceProvider).id,
+    version: ref.read(configProvider).version.text,
+  ),
+);
+
+class UserVaultNotifier extends StateNotifier<UserVault> {
+  UserVaultNotifier({
+    required this.ref,
+    required this.deviceId,
+    required this.version,
+    this.docName = 'vault',
+  }) : super(UserVault());
+  final Ref ref;
+  final String docName;
+  final String deviceId;
+  final String version;
+  DocMeta? meta;
+
+  Future<void> init() async {
+    log('Create doc.', name: _comp);
+    Doc? doc = await ref
+        .read(greenVaultProvider)
+        .updateDoc(
+          name: docName,
+          content: state.toJson(),
+          annotation: {'author': deviceId, 'authorVersion': version},
+        );
+    meta = doc!.meta;
+  }
+
+  Future<void> open() async {
+    Doc? doc = await ref.read(greenVaultProvider).getDoc(name: docName);
+    if (doc == null) {
+      await init();
+    } else {
+      state = UserVault.fromJson(doc.content);
+      meta = doc.meta;
+      log('Loaded doc.', name: _comp);
+    }
+  }
+
+  Future<bool> _updateDoc({
+    required UserVault newState,
+    Map<String, dynamic>? annotation,
+  }) async {
+    Doc? doc = await ref
+        .read(greenVaultProvider)
+        .updateDoc(
+          name: docName,
+          content: newState.toJson(),
+          annotation:
+              annotation ?? {'author': deviceId, 'authorVersion': version},
+        );
+    if (doc == null) {
+      return false;
+    }
+    meta = doc.meta;
+    return true;
+  }
+
+  // get save-to-update copy of vault entry
+  UserVaultEntry? copyEntry({required String id}) {
+    if (!state.entries.containsKey(id)) {
+      return null;
+    }
+
+    return UserVaultEntry.fromJson(json.decode(json.encode(state.entries[id])));
+  }
+
+  Future<AppErr> createEntry(UserVaultEntry entry) async {
+    for (UserVaultEntry val in state.entries.values) {
+      if (entry.id == val.id) {
+        return AppErr.entryExists;
+      }
+
+      if (entry.title == val.title && entry.type == val.type) {
+        return AppErr.titleExists;
+      }
+    }
+
+    log('Adding entry ${entry.title}.', name: _comp);
+    entry.updatedAt = DateTime.now().toUtc();
+    UserVault stateCopy = UserVault.copy(state);
+    stateCopy.entries[entry.id] = entry;
+    stateCopy.populateUsernames();
+    state = stateCopy; // trigger notification
+    return await _updateDoc(newState: stateCopy) ? AppErr.ok : AppErr.io;
+  }
+
+  Future<AppErr> updateEntry(UserVaultEntry entry) async {
+    if (!state.entries.containsKey(entry.id)) {
+      return AppErr.notFound;
+    }
+
+    for (UserVaultEntry val in state.entries.values) {
+      if (entry.title == val.title &&
+          entry.type == val.type &&
+          entry.id != val.id) {
+        return AppErr.titleExists;
+      }
+    }
+
+    log('Updating entry ${entry.title}.', name: _comp);
+    entry.updatedAt = DateTime.now().toUtc();
+    UserVault stateCopy = UserVault.copy(state);
+    stateCopy.entries[entry.id] = entry;
+    stateCopy.populateUsernames();
+    state = stateCopy; // trigger notification
+    return await _updateDoc(newState: stateCopy) ? AppErr.ok : AppErr.io;
+  }
+
+  Future<AppErr> deleteEntry({required String id}) async {
+    if (!state.entries.containsKey(id)) {
+      return AppErr.notFound;
+    }
+    log('Delete entry $id.', name: _comp);
+    UserVault stateCopy = UserVault.copy(state);
+    stateCopy.entries.remove(id);
+    stateCopy.populateUsernames();
+    state = stateCopy; // trigger notification
+    return await _updateDoc(newState: stateCopy) ? AppErr.ok : AppErr.io;
+  }
+
+  List<String> search({required String query}) {
+    String q = query.toLowerCase();
+    List<String> rv = [];
+
+    for (UserVaultEntry entry in state.entries.values) {
+      if (entry.title.toLowerCase().contains(q) ||
+          entry.username.toLowerCase().contains(q) ||
+          entry.notes.toLowerCase().contains(q)) {
+        rv.add(entry.id);
+        continue;
+      }
+      for (String uri in entry.uris) {
+        if (uri.toLowerCase().contains(q)) {
+          rv.add(entry.id);
+        }
+      }
+    }
+    return rv;
+  }
+
+  Map<String, dynamic> export() {
+    return {'content': state.toJson(), 'rev': meta!.revs.export()};
+  }
+
+  bool worthUpdate(Revision rev) {
+    // ignore initial revision
+    if (rev.seq == 1) {
+      return false;
+    }
+
+    // if our revision is an initial one, the other is preferrable
+    if (meta!.revs.current.seq == 1) {
+      return true;
+    }
+
+    if ((meta!.revs.current.hash == rev.hash) ||
+        (meta!.revs.current.date >= rev.date)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  // Import snapshot received from another device.
+  Future<bool> import(Map<String, dynamic> snapshot) async {
+    Revisions revs = Revisions.import(snapshot['rev']);
+
+    if (!worthUpdate(revs.current)) {
+      return false;
+    }
+
+    log('Import doc ${revs.current.hash}.', name: _comp);
+    UserVault content = UserVault.fromJson(snapshot['content']);
+    state = content; // trigger notification
+    return await _updateDoc(
+      newState: content,
+      annotation: {
+        'date': revs.current.date,
+        'author': revs.current.author,
+        'authorHash': revs.current.authorHash,
+        'authorVersion': revs.current.authorVersion,
+      },
+    );
+  }
+
+  Revision get currentRevision {
+    return meta!.revs.current;
+  }
+}
+
+class UserVault {
+  UserVault();
+
+  // deep copy constructor
+  UserVault.copy(UserVault copy) {
+    json.decode(json.encode(copy.entries)).forEach((k, v) {
+      entries[k] = UserVaultEntry.fromJson(v);
+    });
+    populateUsernames();
+  }
+
+  // serialization constructor
+  UserVault.fromJson(Map<String, dynamic> parsedJson) {
+    Map parsedEntries = json.decode(parsedJson['entries']);
+    parsedEntries.forEach((k, v) {
+      entries[k] = UserVaultEntry.fromJson(v);
+    });
+    populateUsernames();
+  }
+
+  Map<String, UserVaultEntry> entries = {};
+
+  // in-memory case-insensitively sorted list of usernames for autocomplete
+  List<String> usernames = [];
+
+  // serialization
+  Map<String, dynamic> toJson() {
+    return {'entries': json.encode(entries)};
+  }
+
+  void populateUsernames() {
+    Set<String> userSet = {};
+
+    entries.forEach((key, value) {
+      if ((value.type == typeLogin) &&
+          value.username.isNotEmpty &&
+          !userSet.contains(value.username)) {
+        userSet.add(value.username);
+      }
+    });
+
+    usernames = userSet.toList()
+      ..sort((a, b) => a.toLowerCase().compareTo(b.toLowerCase()));
+  }
+}
+
+const String typeLogin = 'login';
+const String typeNote = 'note';
+
+class UserVaultEntry {
+  UserVaultEntry({required this.id, required this.type});
+
+  UserVaultEntry.fromJson(Map<String, dynamic> parsedJson) {
+    id = parsedJson['id'];
+    type = parsedJson['type'];
+
+    title = parsedJson['title'];
+    username = parsedJson['username'];
+    password = parsedJson['password'];
+    notes = parsedJson['notes'];
+
+    parsedJson['uris'].forEach((v) {
+      uris.add(v);
+    });
+
+    hidden = parsedJson['hidden'] ?? false;
+    updatedAt = DateTime.parse(parsedJson['updatedAt']);
+  }
+
+  late String id;
+  late String type;
+
+  late String title;
+  late String username;
+  late String password;
+  late String notes;
+  List<String> uris = [];
+  bool hidden = false;
+  DateTime updatedAt = DateTime.now().toUtc();
+
+  // serialization
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'type': type,
+      'title': title,
+      'username': username,
+      'password': password,
+      'notes': notes,
+      'uris': uris,
+      'hidden': hidden,
+      'updatedAt': updatedAt.toIso8601String(),
+    };
+  }
+}
---
+++ test/providers/green_vault/user_vault_provider_test.dart
@@ -0,0 +1,333 @@
+import 'package:zxbase_app/core/ui/app_err.dart';
+import 'package:zxbase_app/providers/config_provider.dart';
+import 'package:zxbase_app/providers/green_vault/device_provider.dart';
+import 'package:zxbase_app/providers/green_vault/green_vault_provider.dart';
+import 'package:zxbase_app/providers/green_vault/user_vault_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zxbase_vault/zxbase_vault.dart';
+import '../../helpers.dart';
+
+void main() {
+  const pwd = '12345678cC%';
+  const entryId = '1';
+  const title = 'Amazon';
+  const username = 'my@gmail.com';
+  const notes = '';
+  const uris = ['https://amazon.com', 'https://google.com'];
+  Map<String, dynamic> snapshot = {};
+  String hash = '';
+
+  cleanupDb();
+  mockPathProvider();
+  test('Init doc', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).init(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    // test the path of opening non existing doc for upgrade scenario
+    // of old versions where user vault didn't exist
+    await userVaultNotifier.open();
+
+    // read the doc
+    var userVault = container.read(userVaultProvider);
+    expect(userVault.entries.length, equals(0));
+
+    expect(
+      userVaultNotifier.currentRevision.name,
+      equals(
+        '1-1125b265e677c4d95f38ab4c1962dab60147b70db0ed62e02d2863bdd0c1f5fc',
+      ),
+    );
+    expect(userVaultNotifier.meta!.revs.current.date, isNot(equals(0)));
+  });
+
+  test('Search', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).open(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    await userVaultNotifier.open();
+
+    // read the doc
+    var userVault = container.read(userVaultProvider);
+    expect(userVault.entries.length, equals(0));
+
+    // add login
+    UserVaultEntry entry = UserVaultEntry(id: entryId, type: typeLogin);
+    entry.title = title;
+    entry.username = username;
+    entry.password = pwd;
+    entry.notes = notes;
+    entry.uris = uris;
+
+    AppErr rv = await userVaultNotifier.createEntry(entry);
+    expect(rv, equals(AppErr.ok));
+
+    // creating same entry should fail
+    UserVaultEntry entryCopy = userVaultNotifier.copyEntry(id: entryId)!;
+    rv = await userVaultNotifier.createEntry(entryCopy);
+    expect(rv, equals(AppErr.entryExists));
+
+    // using same title should fail
+    entryCopy.id = '2';
+    rv = await userVaultNotifier.createEntry(entryCopy);
+    expect(rv, equals(AppErr.titleExists));
+
+    snapshot = userVaultNotifier.export();
+    Revisions rev = Revisions.import(snapshot['rev']);
+    expect(rev.current.seq, equals(2));
+    hash = rev.current.hash;
+
+    // check entry
+    userVault = container.read(userVaultProvider);
+    UserVaultEntry createdEntry = userVault.entries[entryId]!;
+    expect(createdEntry.hidden, equals(false));
+  });
+
+  test('Update entry', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).open(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    await userVaultNotifier.open();
+
+    // read the doc
+    UserVault userVault = container.read(userVaultProvider);
+    expect(userVault.entries.length, equals(1));
+    UserVaultEntry entry = userVault.entries[entryId]!;
+    expect(entry.notes, equals(notes));
+
+    UserVaultEntry entryCopy = userVaultNotifier.copyEntry(id: entryId)!;
+    entryCopy.notes = 'xxx';
+    entryCopy.hidden = true;
+
+    // update entry
+    AppErr rv = await userVaultNotifier.updateEntry(entryCopy);
+    expect(rv, equals(AppErr.ok));
+
+    // check entry
+    entry = userVault.entries[entryId]!;
+    expect(entry.hidden, equals(false));
+  });
+
+  test('Create second entry', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).open(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    await userVaultNotifier.open();
+
+    // add login
+    UserVaultEntry entry = UserVaultEntry(id: '2', type: typeLogin);
+    entry.title = 'Second title';
+    entry.username = 'second@gmail.com';
+    entry.password = pwd;
+    entry.notes = 'second notes';
+    entry.uris = ['https://second.com'];
+    entry.hidden = true;
+
+    AppErr rv = await userVaultNotifier.createEntry(entry);
+    expect(rv, equals(AppErr.ok));
+
+    List<String> entryIds = userVaultNotifier.search(query: 'second');
+    expect(entryIds, equals(['2']));
+
+    entryIds = userVaultNotifier.search(query: 'third');
+    expect(entryIds, equals([]));
+
+    // case insensitive search
+    entryIds = userVaultNotifier.search(query: 'GOOGLE');
+    expect(entryIds, equals(['1']));
+  });
+
+  test('Delete entry', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).open(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    await userVaultNotifier.open();
+
+    // read the doc
+    UserVault userVault = container.read(userVaultProvider);
+    expect(userVault.entries.length, equals(2));
+
+    UserVaultEntry entry = userVault.entries[entryId]!;
+    expect(entry.type, equals(typeLogin));
+    expect(entry.title, equals(title));
+    expect(entry.username, equals(username));
+    expect(entry.password, equals(pwd));
+    expect(entry.notes, equals('xxx'));
+    expect(entry.uris, equals(uris));
+
+    // delete entry
+    AppErr rv = await userVaultNotifier.deleteEntry(id: entryId);
+    expect(rv, equals(AppErr.ok));
+  });
+
+  test('Verify deletion', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).open(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    await userVaultNotifier.open();
+
+    // read the doc
+    UserVault userVault = container.read(userVaultProvider);
+    expect(userVault.entries.length, equals(1));
+    expect(container.read(userVaultProvider).entries[entryId], equals(null));
+  });
+
+  test('Import vault', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).open(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    await userVaultNotifier.open();
+
+    // import of the same version is not allowed
+    bool rv = await userVaultNotifier.import(userVaultNotifier.export());
+    expect(rv, equals(false));
+
+    // import of older version is not allowed
+    rv = await userVaultNotifier.import(snapshot);
+    expect(rv, equals(false));
+
+    // manually update snapshot date
+    Revisions rev = Revisions.import(snapshot['rev']);
+    rev.current.date = DateTime.now().toUtc().millisecondsSinceEpoch;
+    snapshot['rev'] = rev.export();
+    rv = await userVaultNotifier.import(snapshot);
+    expect(rv, equals(true));
+    expect(userVaultNotifier.meta!.revs.current.seq, equals(6));
+    expect(userVaultNotifier.meta!.revs.current.hash, equals(hash));
+    expect(container.read(userVaultProvider).entries.length, equals(1));
+    expect(
+      container.read(userVaultProvider).entries[entryId]!.title,
+      equals(title),
+    );
+  });
+
+  test('Uniqueness', () async {
+    // scope container to a single test
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    // startup sequence
+    await container.read(configProvider).init();
+    await container.read(greenVaultProvider.notifier).open(pwd);
+    await container.read(deviceProvider.notifier).init();
+    UserVaultNotifier userVaultNotifier = container.read(
+      userVaultProvider.notifier,
+    );
+    await userVaultNotifier.open();
+
+    // add login
+    UserVaultEntry entry = UserVaultEntry(id: '10', type: typeLogin);
+    entry.title = 'Unique title';
+    entry.username = 'second@gmail.com';
+    entry.password = pwd;
+    entry.notes = 'second notes';
+    entry.uris = ['https://second.com'];
+    AppErr rv = await userVaultNotifier.createEntry(entry);
+    expect(rv, equals(AppErr.ok));
+
+    UserVaultEntry entry2 = UserVaultEntry(id: '11', type: typeLogin);
+    entry2.title = 'Unique title';
+    entry2.username = 'second@gmail.com';
+    entry2.password = pwd;
+    entry2.notes = 'second notes';
+    entry2.uris = ['https://second.com'];
+    rv = await userVaultNotifier.createEntry(entry2);
+    expect(rv, equals(AppErr.titleExists));
+
+    UserVaultEntry entry3 = UserVaultEntry(id: '10', type: typeNote);
+    entry3.title = 'Unique title';
+    entry3.username = 'second@gmail.com';
+    entry3.password = pwd;
+    entry3.notes = 'second notes';
+    entry3.uris = ['https://second.com'];
+    rv = await userVaultNotifier.createEntry(entry3);
+    expect(rv, equals(AppErr.entryExists));
+
+    UserVaultEntry entry4 = UserVaultEntry(id: '12', type: typeNote);
+    entry4.title = 'Unique title';
+    entry4.username = 'second@gmail.com';
+    entry4.password = pwd;
+    entry4.notes = 'second notes';
+    entry4.uris = ['https://second.com'];
+    rv = await userVaultNotifier.createEntry(entry4);
+    expect(rv, equals(AppErr.ok));
+
+    UserVaultEntry entry5 = UserVaultEntry(id: '12', type: typeNote);
+    entry5.title = 'Unique title';
+    entry5.username = 'second@gmail.com';
+    entry5.password = pwd;
+    entry5.notes = 'second notes';
+    entry5.uris = ['https://second.com'];
+    rv = await userVaultNotifier.createEntry(entry5);
+    expect(rv, equals(AppErr.entryExists));
+
+    UserVaultEntry entry6 = UserVaultEntry(id: '13', type: typeNote);
+    entry6.title = 'Unique title';
+    entry6.username = 'second@gmail.com';
+    entry6.password = pwd;
+    entry6.notes = 'second notes';
+    entry6.uris = ['https://second.com'];
+    rv = await userVaultNotifier.createEntry(entry6);
+    expect(rv, equals(AppErr.titleExists));
+
+    entry6.type = typeLogin;
+    rv = await userVaultNotifier.updateEntry(entry6);
+    expect(rv, equals(AppErr.notFound));
+  });
+}

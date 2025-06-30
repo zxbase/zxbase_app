a1b2d1bb91bcccaa95344db3102eb13b2863220a
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-06-29 18:42:46 -0700

    add channel message

---
+++ lib/core/channel/channel_message.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+const String cmHsChallenge = 'hsChallenge';
+const String cmHsResponse = 'hsResponse';
+const String cmDirectMessage = 'directMessage';
+const String cmDirectReceipt = 'directReceipt';
+const String cmLocation = 'location';
+const String cmHeartbeat = 'hb';
+const String cmVault = 'vault';
+
+class ChannelMessage {
+  ChannelMessage({required this.type, required this.data});
+  ChannelMessage.fromString(String s) {
+    Map<String, dynamic> map = jsonDecode(s);
+    type = map['type'];
+    data = map['data'];
+  }
+
+  late String type;
+  dynamic data;
+
+  String get str {
+    return jsonEncode({'type': type, 'data': data});
+  }
+}

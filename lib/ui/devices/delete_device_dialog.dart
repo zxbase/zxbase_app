b5de894033ecebc9d423d4a3efc15ceaffea11ea
Ariel Tubaltsev <tubaltzev@gmail.com>
2025-07-27 14:29:46 -0700

    add delete device dialog

---
+++ lib/ui/devices/delete_device_dialog.dart
@@ -0,0 +1,30 @@
+import 'package:zxbase_app/providers/connections_provider.dart';
+import 'package:zxbase_app/providers/green_vault/peer_group_provider.dart';
+import 'package:zxbase_app/providers/green_vault/peers_provider.dart';
+import 'package:zxbase_app/providers/ui_providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:zxbase_flutter_ui/zxbase_flutter_ui.dart';
+
+Future<void> deletePeer({
+  required WidgetRef ref,
+  required String peerId,
+}) async {
+  if (UI.isMobile) {
+    _clearSelectedPeer(ref);
+  }
+
+  // close connection, delete convos, location, vault group
+  await ref.read(connectionsProvider).deleteConnection(peerId: peerId);
+  await ref.read(peersProvider.notifier).deletePeer(peerId: peerId);
+  await ref
+      .read(peerGroupsProvider.notifier)
+      .deleteVaultGroupPeer(peerId: peerId);
+
+  if (UI.isDesktop) {
+    _clearSelectedPeer(ref);
+  }
+}
+
+void _clearSelectedPeer(WidgetRef ref) {
+  ref.read(selectedDeviceProvider.notifier).state = '';
+}
